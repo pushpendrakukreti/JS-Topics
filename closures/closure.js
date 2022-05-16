36411d5c96d36740fe8338bcbbe247bd560d6d75
Pushpendra Kukreti <41129279+pushpendrakukreti@users.noreply.github.com>
2022-05-17 02:19:41 +0530

    added closures concept

---
+++ closures/closure.js
@@ -0,0 +1,30 @@
+//Create a counter function which has increment and getValue functionality
+/*----------------------------------*/
+const privateCounter = () => {
+    let count = 0;
+
+    return {
+        increment: (val = 4) => {
+            count += val;
+        },
+        getValue: () =>{
+            return count;
+        },
+    };
+};
+
+const counter = privateCounter();
+console.log(counter.getValue());
+counter.increment();
+console.log(counter.getValue());
+/*-----------------------------------*/
+
+/*-----------------------------------*/
+const privateSecret = () => {
+    const secret = "foo";
+
+    return () => secret;
+};
+const getSecret = privateSecret();
+console.log(getSecret());
+/*-----------------------------------*/
