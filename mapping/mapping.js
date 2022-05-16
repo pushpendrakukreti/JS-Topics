84ad5c9e091a6f3fc038cade5a390f7a770407ea
Pushpendra Kukreti <41129279+pushpendrakukreti@users.noreply.github.com>
2022-05-17 02:19:06 +0530

    added map(), sort(), filter() concept

---
+++ mapping/mapping.js
@@ -0,0 +1,39 @@
+const users = [
+    {
+        id: 1,
+        name: "Jack",
+        isActive: false,
+        age: 30
+    },
+    {
+        id: 2,
+        name: "Harry",
+        isActive: true,
+        age: 20
+    },
+    {
+        id: 3,
+        name: "Lucy",
+        isActive: true,
+        age: 28
+    }
+];
+
+// const names = [];
+/*Using for loop*/
+// for (let i = 0; i < users.length; i++) {
+//     names.push(users[i].name);
+// }
+
+/*Using forEach loop*/
+// users.forEach(user=>{
+//    names.push(user.name); 
+// });
+
+/*Using map function*/
+const names = users
+    .sort((user1, user2) => (user1.age < user2.age) ? 1 : -1)
+    .filter((user) => user.isActive)
+    .map((user) => user.name);
+
+console.log(names);
