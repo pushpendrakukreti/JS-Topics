398976b1e6df42e6ceef6e907f3be21fca2d113a
Pushpendra Kukreti <41129279+pushpendrakukreti@users.noreply.github.com>
2022-05-17 02:17:09 +0530

    added hoisting concept

---
+++ hoisting/hoisting.js
@@ -0,0 +1,19 @@
+/*Hoisting means the declaration of variables and functions bubble up
+to the top*/
+
+//console.log(foo);  //foo is not defined
+//foo=1;  //declaration with no type
+
+//console.log(foo);  //undefined
+//var foo = 2;  //it will bubble up to top
+
+foo = 3; //foo will be 3
+console.log(foo);  //3
+var foo; //it will bubble up to top
+
+func();   //foo called!!
+function func(){      //this function will bubble up to top
+    console.log('func called!!');
+}
+
+//let and const will not bubble up and work for same line when declared
