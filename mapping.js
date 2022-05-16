23d62a0346623834c454b00f3dbb690c28941e8c
Pushpendra Kukreti <41129279+pushpendrakukreti@users.noreply.github.com>
2022-05-17 00:33:05 +0530

    uncommented filter() function

--- mapping.js
+++ mapping.js
@@ -33,7 +33,7 @@ const users = [
 /*Using map function*/
 const names = users
     .sort((user1, user2) => (user1.age < user2.age) ? 1 : -1)
-    // .filter((user) => user.isActive)
+    .filter((user) => user.isActive)
     .map((user) => user.name);
 
 console.log(names);
