17e7e5c6bd4c4d211001faebd6e517f9983e7101
Pushpendra Kukreti <41129279+pushpendrakukreti@users.noreply.github.com>
2022-05-17 02:18:17 +0530

    added null-vs-undefined concept

---
+++ null-vs-undefined/null-vs-undefined.js
@@ -0,0 +1,7 @@
+let var1;           //declared but not initialized
+console.log(var1);  //undefined
+console.log(typeof var1);   //undefined
+
+let var2 = null;   //declared and also initialized
+console.log(var2); //null
+console.log(typeof var2); //object
