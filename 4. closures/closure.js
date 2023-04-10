f546fa5c828fa2088a260f39d466f6e8f248696e
Pushpendra Kukreti <41129279+pushpendrakukreti@users.noreply.github.com>
2023-04-10 23:37:12 +0530

    closure : description added for a closure

--- 4. closures/closure.js
+++ 4. closures/closure.js
@@ -1,13 +1,22 @@
+/* 
+    A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment).
+    In other words, a closure gives you access to an outer function's scope from an inner function.
+*/
+
 //Create a counter function which has increment and getValue functionality
-/*----------------------------------*/
+
+/*----Global Execution Context----*/
 const privateCounter = () => {
+    // Execution Context(privateCounter())
     let count = 0;
 
     return {
         increment: (val = 1) => {
+        // Execution Context(increment())
             count += val;
         },
-        getValue: () =>{
+        getValue: () => {
+        // Execution Context(getValue())
             return count;
         },
     };
